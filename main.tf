b3c024aa47d053b7c80dc1f5cb3fd296e2922851
fozziiee <kaydefoster@gmail.com>
2025-06-29 14:39:28 +1000

    Add Network Security Group and outputs for NSG association

--- main.tf
+++ main.tf
@@ -24,3 +24,40 @@ resource "azurerm_subnet" "cyberlab_subnet" {
   address_prefixes = ["10.0.1.0/24"]
 }
 
+# Create Network Security Group (NSG) 
+resource "azurerm_network_security_group" "cyberlab_nsg" {
+  name = "Cyberlab-NSG"
+  location = azurerm_resource_group.cyberlab-rg.location
+  resource_group_name = azurerm_resource_group.cyberlab-rg.name
+
+  security_rule {
+    name                        = "Allow-RDP"
+    priority                    = 100
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = "3389"
+    source_address_prefix       = "Internet"
+    destination_address_prefix  = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG with the Subnet
+resource "azurerm_subnet_network_security_group_association" "cyberlab_assoc" {
+  subnet_id                     = azurerm_subnet.cyberlab_subnet.id
+  network_security_group_id     = azurerm_network_security_group.cyberlab_nsg.id
+}
+
--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,10 @@ output "vnet_name" {
   value = azurerm_virtual_network.cyberlab-vnet.name
 }
 
+output "nsg_name" {
+  value = azurerm_network_security_group.cyberlab_nsg.name
+}
+
+output "subnet_nsg_assoc_id" {
+  value = azurerm_subnet_network_security_group_association.cyberlab_assoc.id
+}
