b8267b7f5841c1526f3db3ab78f646ff79ee4ddb
fozziiee <kaydefoster@gmail.com>
2025-06-29 15:03:29 +1000

    Add public IP, network interface, and Windows VM resources; create .gitignore and variables file

--- main.tf
+++ main.tf
@@ -61,3 +61,51 @@ resource "azurerm_subnet_network_security_group_association" "cyberlab_assoc" {
   network_security_group_id     = azurerm_network_security_group.cyberlab_nsg.id
 }
 
+# Create a public IP address
+resource "azurerm_public_ip" "cyberlab_pip" {
+  name = "Cyberlab-PublicIP"
+  location = azurerm_resource_group.cyberlab-rg.location
+  resource_group_name = azurerm_resource_group.cyberlab-rg.name
+  allocation_method = "Dynamic"
+}
+
+# Create a network interface
+resource "azurerm_network_interface" "cyberlab-nic" {
+  name = "Cyberlab-NIC"
+  location = azurerm_resource_group.cyberlab-rg.location
+  resource_group_name = azurerm_resource_group.cyberlab-rg.name
+
+  ip_configuration {
+    name = "internal"
+    subnet_id = azurerm_subnet.cyberlab_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.cyberlab_pip.id
+  }
+}
+
+# Deploy the Windows Server VM
+resource "azurerm_windows_virtual_machine" "cyberlab_vm" {
+  name = "Cyberlab-VM"
+  location = azurerm_resource_group.cyberlab-rg.location
+  resource_group_name = azurerm_resource_group.cyberlab-rg.name
+  size = "Standard_B1ms"
+  admin_username = var.admin_username
+  admin_password = var.admin_password
+
+  network_interface_ids = [
+    azurerm_network_interface.cyberlab-nic.id
+  ]
+
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer = "WindowsServer"
+    sku = "2019-Datacenter"
+    version = "latest"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "admin_username" {
+  description = "Admin username for the Windows VM"
+  type = string
+}
+
+variable "admin_password" {
+  description = "Admin password for the Windows VM"
+  type = string
+  sensitive = true
+}
