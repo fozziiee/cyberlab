8d25aafb67c82087a3ad7866674408b05cf31ac1
fozziiee <kaydefoster@gmail.com>
2025-07-06 20:44:28 +1000

    Enabled script ext to enable winRM

--- enable-winrm.ps1
+++ enable-winrm.ps1
@@ -1,12 +1,5 @@
 # enable-winrm.ps1
 
-# Configure WinRM
-winrm quickconfig -quiet
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
-
-# Enable Powershell Remoting
+Set-Item -Path "WSMan:\localhost\Service\AllowUnencrypted" -Value true
+Set-Item -Path "WSMan:\localhost\Service\Auth\Basic" -Value true
 Enable-PSRemoting -Force
-
-# Allow WinRM HTTPS through Firewall
-New-NetFirewallRule -Name "WinRMHTTPS" -DisplayName "WinRM over HTTPS" -Enabled True -Direction Inbound -Protocol TCP -LocalPort 5986 -Action Allow 
